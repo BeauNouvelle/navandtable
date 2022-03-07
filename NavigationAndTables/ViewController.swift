192c4cb0137f4bc194702b4c58a7ea6f9d4b9734
Beau Nouvelle <beau@getswifty.dev>
2022-03-07 18:48:48 +1100

    Initial Commit

---
+++ NavigationAndTables/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NavigationAndTables
+//
+//  Created by Beau Nouvelle on 7/3/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
